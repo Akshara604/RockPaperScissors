9bcb59a8c4f1e2377c6b74a67af1ec360d20f4b6
Akshara S PA <74604911+Akshara604@users.noreply.github.com>
2021-08-31 20:53:26 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomNo1 = Math.floor(Math.random() *3)+1;
+var randomImg1 = "images/img" + randomNo1 +".jpg";
+var image1 = document.querySelectorAll("img")[0]
+image1.setAttribute("src", randomImg1);
+
+var randomNo2 = Math.floor(Math.random() *3)+1;
+var randomImg2 = "images/img" + randomNo2 +".jpg";
+var image2 = document.querySelectorAll("img")[1]
+image2.setAttribute("src", randomImg2);
+
+if(randomNo1 == 1 && randomNo2 == 2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(randomNo1 == 2 && randomNo2 == 3){
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(randomNo1 == 3 && randomNo2 == 1){
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(randomNo1 ==  randomNo2 ){
+    document.querySelector("h1").innerHTML = "Draw";
+}
+else{
+    document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
